05d7187116d4712ab48458825b90740035a5ba7f
Kenneth Lau <kennethlau0759@gmail.com>
2021-12-30 15:54:02 +0000

    made classes immutable to improve security

--- src/Adventure/Location.java
+++ src/Adventure/Location.java
@@ -8,15 +8,21 @@ public class Location {
 	private final String description;
 	private final Map<String, Integer> exits;
 	
-	public Location(int locationID, String description) {
+	public Location(int locationID, String description, Map<String , Integer> exits) {
 		this.locationID = locationID;
 		this.description = description;
-		this.exits = new HashMap<String, Integer>();
+//		creating a copy so when you assign stuff using a temp hashmap, using remove doesn't affect the stored map
+		
+		if (exits != null){
+			this.exits = new HashMap<>(exits);
+		}else{
+			this.exits = new HashMap<>();
+		}
 		exits.put("Q", 0);
 	}
-	public void addExit(String direction, int location){
-		exits.put(direction, location);
-	}
+//	public void addExit(String direction, int location){
+//		exits.put(direction, location);
+//	}
 	public int getLocationID() {
 		return locationID;
 	}
