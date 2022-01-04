6dfc21b703c476cc022b951550e83fc49aa92378
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-04 11:36:35 +0000

    afjusting location , read writing from file, using buffered read

---
+++ src/IO/Location.java
@@ -0,0 +1,42 @@
+package IO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+	private final int locationID;
+	private final String description;
+	private final Map<String, Integer> exits;
+	
+	public Location(int locationID, String description, Map<String , Integer> exits) {
+		this.locationID = locationID;
+		this.description = description;
+//		creating a copy so when you assign stuff using a temp hashmap, using remove doesn't affect the stored map
+		
+		if (exits != null){
+			this.exits = new HashMap<>(exits);
+		}else{
+			this.exits = new HashMap<>();
+		}
+		exits.put("Q", 0);
+	}
+//	public void addExit(String direction, int location){
+//		exits.put(direction, location);
+//	}
+	public int getLocationID() {
+		return locationID;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Map<String, Integer> getExits() {
+//		as it returns a copy, you can't modify the original hashmap
+		return new HashMap<String, Integer>(exits);
+	}
+	
+	protected void addExit(String direction, int location){
+		exits.put(direction,location);
+	}
+}
--- src/IO/Main.java
+++ src/IO/Main.java
@@ -1,7 +1,69 @@
 package IO;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+	private static Locations locations = new Locations();
+	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		location ID matches a location description
+		
+		Map<String, String> directions = new HashMap<>();
+		directions.put("WEST", "W");
+		directions.put("EAST", "E");
+		directions.put("SOUTH", "S");
+		directions.put("NORTH", "N");
+		directions.put("QUIT", "Q");
+		
+		int loc = 1;
+		while (true) {
+//			get description from location field.
+			System.out.println(locations.get(loc).getDescription());
+//			if you choose exit, it ends game
+			if (loc == 0) {
+				break;
+			}
+//			retrieve map of viable directions
+			Map<String, Integer> exits = locations.get(loc).getExits();
+//			print avaialbe directions
+			System.out.print("Available exits are ");
+			for (String exit : exits.keySet()) {
+				System.out.print(exit + ", ");
+			}
+			System.out.println();
+//			read direction
+			String direction = scanner.nextLine().toUpperCase(Locale.ROOT);
+//			Parse phrases, searches for compass direction words
+//			String[] inputs = input.split(" ");
+//			String direction = "";
+//			for (int i = 0; i <= inputs.length -1; i++) {
+//				if (directions.containsKey(inputs[i])) {
+//					direction = directions.get(inputs[i]);
+//					break;
+//				}
+//			}
+			if (direction.length() > 1) {
+				String[] words = direction.split(" ");
+				for (String word : words) {
+					if (directions.containsKey(word)) {
+						direction = directions.get(word);
+					}
+				}
+			}
+
+//			if valid direction, set location key for locations from exits values to corresponding direction key
+			if (exits.containsKey(direction)) {
+				loc = exits.get(direction);
+			} else {
+				System.out.println("You cant move in that direction");
+				
+			}
+			
+		}
 		
 	}
 }
